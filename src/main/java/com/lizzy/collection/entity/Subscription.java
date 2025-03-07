0a81beb7ba0aac365a60f23481a0ed77d61e997b
gebeen-dev <rrdd1207@gmail.com>
2025-03-07 15:33:11 +0900

    feat : Subscription entity 필드추가

--- src/main/java/com/lizzy/collection/entity/Subscription.java
+++ src/main/java/com/lizzy/collection/entity/Subscription.java
@@ -1,4 +1,20 @@
 package com.lizzy.collection.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Subscription {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private LocalDateTime endDate;
 }
