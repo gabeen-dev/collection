5eb464dbd18a345120a1552ede18bef206eedf83
gebeen-dev <rrdd1207@gmail.com>
2025-03-07 15:32:31 +0900

    feat : Contents entity 필드추가

--- src/main/java/com/lizzy/collection/entity/Contents.java
+++ src/main/java/com/lizzy/collection/entity/Contents.java
@@ -1,4 +1,36 @@
 package com.lizzy.collection.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Contents {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String contentType;
+
+	@Column(nullable = false)
+	private String title;
+
+	@Column(nullable = false)
+	private int viewCount;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column
+	private String url;
+
+	@Column(nullable = false)
+	private double rating;
+
+	@Column(nullable = false)
+	private String status;
 }
