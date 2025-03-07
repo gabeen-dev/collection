61d0b84ad764302240ff20018545a1a35fe4219e
gebeen-dev <rrdd1207@gmail.com>
2025-03-07 15:38:08 +0900

    feat : Users entity 필드 추가

--- src/main/java/com/lizzy/collection/entity/Users.java
+++ src/main/java/com/lizzy/collection/entity/Users.java
@@ -1,4 +1,30 @@
 package com.lizzy.collection.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class Users {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column
+	private String password;
+
+	@Column
+	private String name;
+
+	@Column(nullable = false)
+	private String role;
+
+	@Column(nullable = false)
+	private String status;
 }
