e0be11178b74be4f0daba8e51c41650c2070c177
gebeen-dev <rrdd1207@gmail.com>
2025-03-07 13:21:15 +0900

    first commit

---
+++ src/test/java/com/lizzy/collection/CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizzy.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
